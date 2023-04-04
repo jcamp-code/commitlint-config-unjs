a40428edc463c8601e4f6637c68abcd61d61488b
John Campion <john@brightshore.com>
2023-04-03 22:42:20 -0400

    feat: compatibility with changelogen
    Also adds a wip type for WIP commits that don't hit the changelog.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,158 @@
-export function testAdd(digit: number, second: number): number {
-  return digit + second
+import type { UserConfig } from '@commitlint/types'
+import { RuleConfigSeverity } from '@commitlint/types'
+
+export const Configuration: UserConfig = {
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'body-leading-blank': [RuleConfigSeverity.Warning, 'always'],
+    'body-max-line-length': [RuleConfigSeverity.Error, 'always', 100],
+    'footer-leading-blank': [RuleConfigSeverity.Warning, 'always'],
+    'footer-max-line-length': [RuleConfigSeverity.Error, 'always', 100],
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 100],
+    'subject-case': [
+      RuleConfigSeverity.Error,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [RuleConfigSeverity.Error, 'never'],
+    'subject-full-stop': [RuleConfigSeverity.Error, 'never', '.'],
+    'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
+    'type-empty': [RuleConfigSeverity.Error, 'never'],
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'examples',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'types',
+        'wip',
+      ],
+    ],
+  },
+  prompt: {
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: 'A new feature or enhancement',
+            title: 'Enhancements',
+            emoji: '🚀',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Fixes',
+            emoji: '🩹',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📖',
+          },
+          style: {
+            description:
+              'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            title: 'Styles',
+            emoji: '🎨',
+          },
+          refactor: {
+            description:
+              'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '💅',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '🔥',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '✅',
+          },
+          build: {
+            description:
+              'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+            title: 'Builds',
+            emoji: '📦',
+          },
+          ci: {
+            description:
+              'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+            title: 'Continuous Integrations',
+            emoji: '🤖',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '🏡',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '🗑',
+          },
+          types: {
+            description: 'Changes to typescript types',
+            title: 'Types',
+            emoji: '🌊',
+          },
+          examples: {
+            description: 'Work on examples or demos',
+            title: 'Examples',
+            emoji: '🏀',
+          },
+          wip: {
+            description: 'WIP commits that should not be in the changelog',
+            title: 'WIP Commits',
+            emoji: '⛔️',
+          },
+        },
+      },
+      scope: {
+        description:
+          'What is the scope of this change (e.g. component or file name)',
+      },
+      subject: {
+        description:
+          'Write a short, imperative tense description of the change',
+      },
+      body: {
+        description: 'Provide a longer description of the change',
+      },
+      isBreaking: {
+        description: 'Are there any breaking changes?',
+      },
+      breakingBody: {
+        description:
+          'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+      },
+      breaking: {
+        description: 'Describe the breaking changes',
+      },
+      isIssueAffected: {
+        description: 'Does this change affect any open issues?',
+      },
+      issuesBody: {
+        description:
+          'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      },
+    },
+  },
 }
+
+module.exports = Configuration
